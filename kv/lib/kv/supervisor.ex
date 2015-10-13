364769e58dce2e270c1b6025329c2068c93817c1
Sean Yu <seansu4you87@gmail.com>
2015-10-12 19:43:50 -0700

    Used ETS as a long term storage cache for the registry
    This yielded a host of race conditions and problems.  Adding a cache
    should be something that's carefully considered, otherwise you'll
    introduce lots of problems.
    
    1.  Race conditions with creating a bucket
    2.  Race conditions with deleting a bucket
    3.  Lack of bucket monitoring when restarting a Registry with cached
    state

--- kv/lib/kv/supervisor.ex
+++ kv/lib/kv/supervisor.ex
@@ -7,16 +7,20 @@ defmodule KV.Supervisor do
 
   @manager_name KV.EventManager
   @registry_name KV.Registry
+  @ets_registry_name KV.Registry
   @bucket_sup_name KV.Bucket.Supervisor
 
   def init(:ok) do
+    ets = :ets.new(@ets_registry_name,
+                   [:set, :public, :named_table, {:read_concurrency, true}])
     # `worker` takes a process module, and an array of arguments to pass into
     # `start_link`.  In this case we start up the event manager, and then take
     # the event manager and pass it into the Registry
     children = [
       worker(GenEvent, [[name: @manager_name]]),
       worker(KV.Bucket.Supervisor, [[name: @bucket_sup_name]]),
-      worker(KV.Registry, [@manager_name, @bucket_sup_name, [name: @registry_name]])
+      worker(KV.Registry, [ets, @manager_name,
+                           @bucket_sup_name, [name: @registry_name]])
     ]
 
     supervise(children, strategy: :one_for_one)
